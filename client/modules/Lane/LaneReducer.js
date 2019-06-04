e985f2a072ea16c26e81401c426e1e230a326833
Rafal Pijet <stronglopez@wp.pl>
2019-06-04 11:52:16 +0200

    Add example from 31.5 module

--- client/modules/Lane/LaneReducer.js
+++ client/modules/Lane/LaneReducer.js
@@ -1,14 +1,43 @@
 // Import Actions
-import {  } from './LaneActions';
+import { CREATE_LANE, UPDATE_LANE, DELETE_LANE, EDIT_LANE } from './LaneActions';
+import { CREATE_NOTE, DELETE_NOTE } from '../Note/NoteActions';
 
 // Initial State
-const initialState = {};
+const initialState = [];
 
-const LaneReducer = (state = initialState, action) => {
+const lanes = (state = initialState, action) => {
   switch (action.type) {
+    case CREATE_LANE:
+      return [...state, action.lane];
+    case UPDATE_LANE:
+      return state.map(lane => {
+        return lane.id === action.id ? { ...lane, ...action.laneId } : lane;
+      });
+    case DELETE_LANE:
+      return state.filter(lane => lane.id !== action.id);
+    case EDIT_LANE:
+      return state.map(lane => {
+        return lane.id === action.id ? { editing: true } : { editing: false };
+      });
+    case CREATE_NOTE:
+      return state.map(lane => {
+        if (lane.id === action.laneId) {
+          const notes = [...lane.notes, action.note.id];
+          return { ...lane, notes };
+        }
+        return lane;
+      });
+    case DELETE_NOTE:
+      return state.map(lane => {
+        if (lane.id === action.laneId) {
+          const notes = lane.notes.filter(note => note.noteId !== action.noteId);
+          return { ...lane, notes };
+        }
+        return lane;
+      });
     default:
       return state;
   }
 };
 
-export default LaneReducer;
+export default lanes;
